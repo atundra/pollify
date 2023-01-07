3f7989c7903fe9c818bd575adbcd9ff1e3b2c0fd
pashutk <me@pashutk.ru>
2023-01-07 22:08:32 +0100

    Add daisyui

--- frontend/src/component/home.rs
+++ frontend/src/component/home.rs
@@ -1,4 +1,6 @@
+use crate::router::Route;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
@@ -8,6 +10,9 @@ pub fn home() -> Html {
           <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
             {"dzień dobry szanowni państwo"}
           </h1>
+          <Link<Route> classes="btn btn-primary" to={Route::Create}>
+            {"Create new poll"}
+          </Link<Route>>
         </div>
       </div>
     }
