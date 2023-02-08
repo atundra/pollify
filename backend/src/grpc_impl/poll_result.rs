ccacc0f593c0161ff560cd027dd996f1d4328818
pashutk <me@pashutk.ru>
2023-02-09 00:05:28 +0100

    Add finished_at to response and show it on the page

--- backend/src/grpc_impl/poll_result.rs
+++ backend/src/grpc_impl/poll_result.rs
@@ -1,10 +1,34 @@
 use crate::{model, storage::DATABASE};
-use bson::{doc, oid::ObjectId};
-use common::grpc::poll_service::{PollResultRequest, PollResultResponse};
-use futures::StreamExt;
+use bson::{doc, oid::ObjectId, Document};
+use common::grpc::poll_service::{self, PollResultRequest, PollResultResponse};
+use futures::TryStreamExt;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 use tonic::{Request, Response, Status};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VoteOptionWithVotes {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub ballot_id: ObjectId,
+    pub name: String,
+    pub description: Option<String>,
+    pub sort: i32,
+    pub votes_count: i32,
+}
+
+impl From<VoteOptionWithVotes> for poll_service::PollResultItem {
+    fn from(option: VoteOptionWithVotes) -> Self {
+        poll_service::PollResultItem {
+            vote_option: Some(poll_service::VoteOption {
+                title: option.name,
+                description: option.description,
+                id: option.id.unwrap().to_hex(),
+            }),
+            votes_count: option.votes_count,
+        }
+    }
+}
 
 pub async fn poll_result(
     request: Request<PollResultRequest>,
@@ -30,98 +54,39 @@ pub async fn poll_result(
         .map_err(|_err| Status::internal("DB find ballot error"))?
         .ok_or_else(|| Status::not_found("Can't find ballot for this poll"))?;
 
-    let ballot_id = ballot.id.unwrap();
-
     let pipeline = vec![
-        doc! {
-          "$match": {
-            "ballot_id": ballot_id
-          }
-        },
-        doc! {
-          "$sort": {
-            "sort": 1
-          }
-        },
+        doc! { "$match": { "ballot_id": ballot.id.unwrap() } },
+        doc! { "$sort": { "sort": 1 } },
         doc! {
           "$lookup": {
             "from": "votes",
             "let": { "option_id": "$_id" },
-            "pipeline": [{
-              "$project": {
-                "option_id": {
-                  "$first": "$option_ids",
-                }
-              }
-            }, {
-              "$match": {
-                "$expr": {
-                  "$eq": ["$option_id", "$$option_id"],
-                }
-              }
-            }],
+            "pipeline": [
+              { "$project": { "option_id": { "$first": "$option_ids", } } },
+              { "$match": { "$expr": { "$eq": ["$option_id", "$$option_id"], } } }
+            ],
             "as": "votes",
           }
         },
-        doc! {
-          "$addFields": {
-            "votes_count": {
-              "$size": "$votes"
-            }
-          }
-        },
+        doc! { "$addFields": { "votes_count": { "$size": "$votes" } } },
     ];
 
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct VoteOptionWithVotes {
-        #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-        pub id: Option<ObjectId>,
-        pub ballot_id: ObjectId,
-        pub name: String,
-        pub description: Option<String>,
-        pub sort: i32,
-        pub votes_count: i32,
-    }
-
-    let mut vote_options = db
+    // I wish i could map over cursor using functional combinators
+    let items = db
         .collection::<model::VoteOption>("options")
         .aggregate(pipeline, None)
         .await
-        .map_err(|_err| Status::internal("DB find vote options error"))?;
+        .map_err(|_err| Status::internal("DB aggregate vote options error"))?
+        .try_collect::<Vec<Document>>()
+        .await
+        .map_err(|_err| Status::internal("DB collecting aggreagted vote options error"))?
+        .into_iter()
+        .map(|document| {
+            bson::from_document::<VoteOptionWithVotes>(document)
+                .map_err(|_err| Status::internal("Failed to deserialize vote option with votes"))
+                .map(|vote_option| vote_option.into())
+        })
+        .collect::<Result<Vec<poll_service::PollResultItem>, _>>()?;
 
-    // .try_collect();
-
-    while let Some(result) = vote_options.next().await {
-        let doc: VoteOptionWithVotes =
-            bson::from_document(result.map_err(|_err| Status::internal("huh?"))?)
-                .map_err(|_err| Status::internal("fail"))?;
-        println!("{doc:?}");
-    }
-    // .await;
-    // .map_err(|_err| Status::internal("DB collecting vote options error"))?;
-
-    // let x = db.collection::<model::Vote>("votes").aggregate(
-    //     vec![doc! {
-    //       "$match":
-    //     }],
-    //     None,
-    // );
-
-    // let options: Vec<poll_service::VoteOption> = vote_options_cursor
-    //     .into_iter()
-    //     .map(
-    //         |model::VoteOption {
-    //              id,
-    //              name,
-    //              description,
-    //              ..
-    //          }| poll_service::VoteOption {
-    //             id: id.unwrap().to_hex(),
-    //             title: name,
-    //             description,
-    //         },
-    //     )
-    //     .collect();
-
-    Ok(Response::new(PollResultResponse { items: vec![] }))
+    Ok(Response::new(PollResultResponse { items }))
 }
--- frontend/src/component/poll_result.rs
+++ frontend/src/component/poll_result.rs
@@ -1,8 +1,40 @@
 use crate::{
-    codegen::poll_service::GetPollBySlugResponse, hooks::use_poll_result::use_poll_result,
+    async_data::AsyncData,
+    codegen::poll_service::{GetPollBySlugResponse, PollResultItem},
+    hooks::use_poll_result::use_poll_result,
     poll_kind::poll_kind_id_to_label,
 };
+use chrono::prelude::*;
 use yew::prelude::*;
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct PollResultTableProps {
+    pub data: Vec<PollResultItem>,
+}
+
+#[function_component(PollResultTable)]
+pub fn poll_results_table(PollResultTableProps { data }: &PollResultTableProps) -> Html {
+    let content: Html = data
+        .iter()
+        .map(|item| {
+            let vote_option = item.vote_option.clone().unwrap();
+            html! {
+              <div>
+                <div>{vote_option.title}{" - "}{item.votes_count}</div>
+                if let Some(description) = vote_option.description {
+                  <div>{description}</div>
+                }
+              </div>
+            }
+        })
+        .collect();
+
+    html! {
+      <div>
+        {content}
+      </div>
+    }
+}
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct PollResultProps {
@@ -12,7 +44,19 @@ pub struct PollResultProps {
 #[function_component(PollResult)]
 pub fn poll_result(PollResultProps { poll }: &PollResultProps) -> Html {
     let poll = poll.clone();
-    let _result_items = use_poll_result(poll.id);
+    let result_items = use_poll_result(poll.id);
+
+    let content = match result_items {
+        AsyncData::Idle => html! { <div>{"Loading"}</div> },
+        AsyncData::Loading => html! { <div>{"Loading"}</div> },
+        AsyncData::Failed(err) => html! { <div>{"Error: "}{err}</div> },
+        AsyncData::Loaded(data) => html! { <PollResultTable {data} /> },
+    };
+
+    let finished_at = poll
+        .finished_at
+        .map(|date| Utc.timestamp_opt(date.seconds, 0).unwrap())
+        .map(|date| format!("{}", date.format("%e %b %Y at %H:%M")));
 
     html! {
       <div class="my-8 space-y-8 w-full">
@@ -20,8 +64,11 @@ pub fn poll_result(PollResultProps { poll }: &PollResultProps) -> Html {
         if let Some(kind) = poll.kind.map(|kind| poll_kind_id_to_label(kind.id)) {
           <p>{kind}</p>
         }
+        if let Some(finished_at) = finished_at {
+          <p>{"Finished "}{finished_at}</p>
+        }
         <div>
-        //   {rows}
+          {content}
         </div>
       </div>
     }
