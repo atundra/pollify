64f85e2c19bbdf697702516842758d82a2d15ca3
pashutk <me@pashutk.ru>
2023-02-09 21:05:16 +0100

    UI done

--- frontend/src/component/poll_result.rs
+++ frontend/src/component/poll_result.rs
@@ -1,7 +1,10 @@
 use crate::{
     async_data::AsyncData,
     codegen::poll_service::{GetPollBySlugResponse, PollResultItem},
-    hooks::use_poll_result::use_poll_result,
+    hooks::{
+        use_poll_result::use_poll_result,
+        use_toast_on_async_data_error::use_toast_on_async_data_error,
+    },
     poll_kind::poll_kind_id_to_label,
 };
 use chrono::prelude::*;
@@ -14,17 +17,55 @@ pub struct PollResultTableProps {
 
 #[function_component(PollResultTable)]
 pub fn poll_results_table(PollResultTableProps { data }: &PollResultTableProps) -> Html {
+    let total_votes: i32 = data.iter().map(|item| item.votes_count).sum();
     let content: Html = data
         .iter()
-        .map(|item| {
-            let vote_option = item.vote_option.clone().unwrap();
-            html! {
-              <div>
-                <div>{vote_option.title}{" - "}{item.votes_count}</div>
-                if let Some(description) = vote_option.description {
-                  <div>{description}</div>
+        .enumerate()
+        .map(|(index, item)| {
+            let progress_classes = classes!(
+                "progress",
+                "w-full",
+                match index {
+                    0 => "progress-primary",
+                    _ => "progress-secondary",
                 }
+            );
+            let ratio = item.votes_count as f32 / total_votes as f32;
+            let vote_option = item.vote_option.clone().unwrap();
+            
+            // Pretty print percent value
+            let percent = format!("{}", ((ratio * 10000.0).round() as i32) as f32 / 100.0);
+
+            let votes_string = match item.votes_count {
+              1 => " vote",
+              _ => " votes",
+            };
+
+            html! {
+              <div class="flex items-center gap-x-4">
+                <div class="basis-1/2 space-y-2">
+                  <div class="text-2xl font-bold">{vote_option.title}</div>
+                  if let Some(description) = vote_option.description {
+                    <div class="text-slate-500">{description}</div>
+                  }
+                </div>
+                <div class="basis-1/2">
+                  <div class="flex justify-between">
+                    <div>{percent}{"%"}</div>
+                    <div>{item.votes_count}{votes_string}</div>
+                  </div>
+
+                  <progress class={progress_classes} value={(ratio * 100.0).to_string()} max="100"></progress>
+                </div>
               </div>
+            }
+        })
+        .enumerate()
+        .flat_map(|(index, item)| {
+            if index == 0 {
+                vec![item]
+            } else {
+                vec![html! { <div class="divider"></div> }, item]
             }
         })
         .collect();
@@ -45,6 +86,7 @@ pub struct PollResultProps {
 pub fn poll_result(PollResultProps { poll }: &PollResultProps) -> Html {
     let poll = poll.clone();
     let result_items = use_poll_result(poll.id);
+    use_toast_on_async_data_error(result_items.clone());
 
     let content = match result_items {
         AsyncData::Idle => html! { <div>{"Loading"}</div> },
